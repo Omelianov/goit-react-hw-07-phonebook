6caa8346c35a5690388dca589c593734589049f5
Omelianov Nicolai <omelianov.n@gmail.com>
2023-06-25 02:11:44 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contactsOperations';
+import AddForm from './AddForm/AddForm';
+import css from './App.module.css';
+
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.container}>
+      <h1 className={css.title}>Phonebook</h1>
+      <AddForm />
+      <h2 className={css.contacts}>Contacts</h2>
+      <Filter />
+      <ContactList />
     </div>
   );
 };
+
+
+export default App;
